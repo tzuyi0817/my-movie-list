58781814033c14746f53f0aef1b765769afbd7b3
Tzuyi Yang <a0988426059@gmail.com>
2019-03-05 21:21:53 +0800

    favorite.html

---
+++ index.js
@@ -0,0 +1,132 @@
+(function () {
+  const BASE_URL = 'https://movie-list.alphacamp.io'
+  const INDEX_URL = BASE_URL + '/api/v1/movies/'
+  const POSTER_URL = BASE_URL + '/posters/'
+  const data = []
+  const dataPanel = document.querySelector('#data-panel')
+
+  const searchBtn = document.getElementById('submit-search')
+  const searchInput = document.getElementById('search')
+
+  const pagination = document.getElementById('pagination')
+  const ITEM_PER_PAGE = 12
+  let paginationData = []
+
+  axios
+    .get(INDEX_URL)
+    .then((response) => {
+      data.push(...response.data.results)
+      // displayDataList(data)
+      getTotalPages(data)
+      getPageData(1, data)
+    })
+    .catch(error => console.log(error))
+
+  dataPanel.addEventListener('click', event => {
+    if (event.target.matches('.btn-show-movie')) {
+      showMovie(event.target.dataset.id)
+    } else if (event.target.matches('.btn-add-favorite')) {
+      addFavoriteItem(event.target.dataset.id)
+    }
+  })
+
+  // listen to search btn click event
+  searchBtn.addEventListener('click', event => {
+    let results = []
+    event.preventDefault()
+
+    const regex = new RegExp(searchInput.value, 'i')
+    results = data.filter(movie => movie.title.match(regex))
+    console.log(results)
+    dgetTotalPage(results)
+    getPageData(1, results)
+  })
+
+  // listen to pagination click event
+  pagination.addEventListener('click', event => {
+    console.log(event.target.dataset.page)
+    if (event.target.tagName === 'A') {
+      getPageData(event.target.dataset.page)
+    }
+  })
+
+  function displayDataList(data) {
+    let htmlContent = ''
+    data.forEach(function (item, index) {
+      htmlContent += `
+      <div class="col-sm-3">
+        <div class="card mb-2">
+          <img class="card-img-top" src="${POSTER_URL}${item.image}" alt="Card image cap">
+
+          <div class="card-body movie-item-body">
+            <h6>${item.title}</h6>
+          </div>
+
+          <div class="card-footer">
+            <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
+            <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
+          </div>
+        </div>
+      </div>
+    `
+    })
+    dataPanel.innerHTML = htmlContent
+  }
+
+  function showMovie(id) {
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-data')
+    const modalDescription = document.getElementById('show-movie-description')
+
+    const url = INDEX_URL + id
+    console.log(url)
+
+    axios
+      .get(url)
+      .then(response => {
+        const data = response.data.results
+        console.log(data)
+
+        modalTitle.textContent = data.title
+        modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+        modalDate.textContent = `release at : ${data.release_date}`
+        modalDescription.textContent = `${data.description}`
+      })
+  }
+
+  function addFavoriteItem(id) {
+    const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+    const movie = data.find(item => item.id === Number(id))
+
+    if (list.some(item => item.id === Number(id))) {
+      alert('${movie.title} is already in your favorite list.')
+    } else {
+      list.push(movie)
+      alert('Added ${movie.title} to your favorite list!')
+    }
+    localStorage.setItem('favoriteMovies', JSON.stringify(list))
+  }
+
+  function getTotalPages(data) {
+    let totalPages = Math.ceil(data.length / ITEM_PER_PAGE) || 1
+    let pageItemConetnt = ""
+
+    for (let i = 0; i < totalPages; i++) {
+      pageItemConetnt += `
+      <li class="page-item">
+        <a class="page-link" href="javascript:;" data-page="${i + 1}">${i + 1}</a>
+      </li>
+      `
+    }
+    pagination.innerHTML = pageItemConetnt
+  }
+
+  function getPageData(pageNum, data) {
+    paginationData = data || paginationData
+    let offset = (pageNum - 1) * ITEM_PER_PAGE
+    let pageData = paginationData.slice(offset, offset + ITEM_PER_PAGE)
+    displayDataList(pageData)
+  }
+})()
+
